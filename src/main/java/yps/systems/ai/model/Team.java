4a9c9c67c0c1153e0fbfae8a6c2a6e49fd431eff
ShanderSandoval <135709230+ShanderSandoval@users.noreply.github.com>
2025-02-18 13:33:39 -0500

    Update Team.java

--- src/main/java/yps/systems/ai/model/Team.java
+++ src/main/java/yps/systems/ai/model/Team.java
@@ -3,6 +3,7 @@ package yps.systems.ai.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,7 +27,9 @@ public class Team {
     @JsonAlias({"id", "elementId"})
     private String id;
     private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private Boolean state;
     private String leaderElementId;
